0589c8549ab99027d67965069841749a7067ae28
stim.ai <leytzher@gmail.com>
2021-10-23 13:27:23 +0000

    Level 3 routines for general and symmetric matrix multiplication

--- src/blas_level3.lisp
+++ src/blas_level3.lisp
@@ -1,7 +1,11 @@
 (in-package :openblas)
 
 ;;;; BLAS LEVEL 3
+;;;;
 
+;; General Matrix-matrix multiply
+
+;;;  single
 (defun sgemm (order trans-a trans-b m n k alpha a lda b ldb beta c ldc)
   (cffi:with-pointer-to-vector-data (ptr-a a)
     (cffi:with-pointer-to-vector-data (ptr-b b)
@@ -14,3 +18,95 @@
                                   beta
                                   ptr-c ldc)))))
 
+;;;  double
+(defun dgemm (order trans-a trans-b m n k alpha a lda b ldb beta c ldc)
+  (cffi:with-pointer-to-vector-data (ptr-a a)
+    (cffi:with-pointer-to-vector-data (ptr-b b)
+      (cffi:with-pointer-to-vector-data (ptr-c c)
+        (openblas.ffi:cblas-dgemm order trans-a trans-b
+                                  m n k
+                                  alpha
+                                  ptr-a lda
+                                  ptr-b ldb
+                                  beta
+                                  ptr-c ldc)))))
+                                  
+;;; complex
+(defun cgemm (order trans-a trans-b m n k alpha a lda b ldb beta c ldc)
+  (cffi:with-pointer-to-vector-data (ptr-a a)
+    (cffi:with-pointer-to-vector-data (ptr-b b)
+      (cffi:with-pointer-to-vector-data (ptr-c c)
+        (openblas.ffi:cblas-cgemm order trans-a trans-b
+                                  m n k
+                                  alpha
+                                  ptr-a lda
+                                  ptr-b ldb
+                                  beta
+                                  ptr-c ldc)))))
+                                  
+;;; double-complex
+(defun zgemm (order trans-a trans-b m n k alpha a lda b ldb beta c ldc)
+  (cffi:with-pointer-to-vector-data (ptr-a a)
+    (cffi:with-pointer-to-vector-data (ptr-b b)
+      (cffi:with-pointer-to-vector-data (ptr-c c)
+        (openblas.ffi:cblas-zgemm order trans-a trans-b
+                                  m n k
+                                  alpha
+                                  ptr-a lda
+                                  ptr-b ldb
+                                  beta
+                                  ptr-c ldc)))))
+                                  
+;; Symmetric matrix-matrix multiply
+;;; 
+;;; single
+(defun ssymm (order side uplo m n alpha a lda b ldb beta c ldc)
+  (cffi:with-pointer-to-vector-data (ptr-a a)
+    (cffi:with-pointer-to-vector-data (ptr-b b)
+      (cffi:with-pointer-to-vector-data (ptr-c c)
+        (openblas.ffi:cblas-ssymm order side uplo
+                                  m n
+                                  alpha
+                                  ptr-a lda
+                                  ptr-b ldb
+                                  beta
+                                  ptr-c ldc)))))
+
+;;;  double
+(defun dsymm (order side uplo m n alpha a lda b ldb beta c ldc)
+  (cffi:with-pointer-to-vector-data (ptr-a a)
+    (cffi:with-pointer-to-vector-data (ptr-b b)
+      (cffi:with-pointer-to-vector-data (ptr-c c)
+        (openblas.ffi:cblas-dsymm order side uplo
+                                  m n
+                                  alpha
+                                  ptr-a lda
+                                  ptr-b ldb
+                                  beta
+                                  ptr-c ldc)))))
+
+;;;  complex
+(defun csymm (order side uplo m n alpha a lda b ldb beta c ldc)
+  (cffi:with-pointer-to-vector-data (ptr-a a)
+    (cffi:with-pointer-to-vector-data (ptr-b b)
+      (cffi:with-pointer-to-vector-data (ptr-c c)
+        (openblas.ffi:cblas-csymm order side uplo
+                                  m n
+                                  alpha
+                                  ptr-a lda
+                                  ptr-b ldb
+                                  beta
+                                  ptr-c ldc)))))
+
+;;;  double-complex
+(defun zsymm (order side uplo m n alpha a lda b ldb beta c ldc)
+  (cffi:with-pointer-to-vector-data (ptr-a a)
+    (cffi:with-pointer-to-vector-data (ptr-b b)
+      (cffi:with-pointer-to-vector-data (ptr-c c)
+        (openblas.ffi:cblas-zsymm order side uplo
+                                  m n
+                                  alpha
+                                  ptr-a lda
+                                  ptr-b ldb
+                                  beta
+                                  ptr-c ldc)))))
